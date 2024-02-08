6afb92b0385a837b904461dc14b59a0010aa6726
CapinAhab <you@example.com>
2024-02-08 08:05:42 +0000

    Added satic content with rocket

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use] extern crate rocket;
+#[cfg(test)] mod tests;
+
+use rocket::fs::{FileServer, relative};
+
+//Sets up static file paths
+mod manual {
+    use std::path::{PathBuf, Path};
+    use rocket::fs::NamedFile;
+
+    #[rocket::get("/<path..>")]
+    pub async fn file_path(path: PathBuf) -> Option<NamedFile> {
+        let mut path = Path::new(super::relative!("static")).join(path);
+        if path.is_dir() {
+            path.push("pages/index.html");
+        }
+
+        NamedFile::open(path).await.ok()
+    }
+}
+
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .mount("/", routes![manual::file_path])
+        .mount("/", FileServer::from(relative!("static")))
 }
