baad308daa5e1dffffcae39a9472c595826fc3da
CapinAhab <hazzar.maynard@protonmail.com>
2024-03-22 16:49:01 +0000

    Need to rethink build system

--- src/monitor_network.rs
+++ src/monitor_network.rs
@@ -1,5 +1,5 @@
 use pcap::{Device, Capture};
-use etherparse::SlicedPacket;
+use etherparse::{SlicedPacket,TransportSlice};
 use etherparse::err::packet::SliceError;
 use libc;
 use serde::{Serialize, Deserialize};
@@ -23,7 +23,19 @@ pub struct PacketData{
 pub struct FrontEndPacketData{
     source: [u8; 6],
     destination: [u8; 6],
-    protocole: u16
+    protocole: u16,
+    source_port: u16,
+    destination_port: u16,
+    sequence_number: u32,
+    acknowledgment_number: u32,
+    syn_flag: bool,
+    ack_flag: bool,
+    fin_flag: bool,
+    rst_flag: bool,
+    psh_flag: bool,
+    urg_flag: bool,
+    header_len: usize
+    
 }
 
 //Object that handles listening on the network
@@ -44,27 +56,8 @@ impl NetworkHandler{
 	}
     }
 
-    //Continuously gets packets
-    //Code is for reference, using get_one_packet instead
-    /*
-    pub fn get_packets(mut self) -> Result<FrontEndPacketData, SliceError>{
-	while let Ok(packet) = self.cap.next_packet(){
-	    let frame = packet.data.to_vec();
-	    match Self::process_packet(frame){
-		Err(value) => {
-		    return Err(value)
-		}
-		Ok(value) => {
-		    return Ok(value)
-		}
-	    }
-	}
-	return Ok(create_error_packet(String::from("No packets :(")))
-    }
-     */
-
     //Gets packets and returns simplified struct to front end
-    pub fn get_one_packet_front_end(mut self) -> Result<FrontEndPacketData, SliceError>{
+    pub fn get_one_packet_front_end(mut self) -> Result<FrontEndPacketData, bool>{
 	let packet = self.cap.next_packet();
 	let frame = packet.unwrap().to_vec();
 	match Self::process_packet(frame){
@@ -78,18 +71,38 @@ impl NetworkHandler{
     }
 
     //Converts packet into a struct for user front end
-    fn process_packet(frame :Vec<u8>) -> Result<FrontEndPacketData, SliceError>{ 
+    fn process_packet(frame :Vec<u8>) -> Result<FrontEndPacketData, bool>{ 
 	match SlicedPacket::from_ethernet(&frame) {
 	    Err(value) => {
-		return Err(value)
+		return Err(false)
 	    },
 	    Ok(value) => {
-		let packet = FrontEndPacketData{
-		    source: value.link.clone().unwrap().to_header().unwrap().source,
-		    destination: value.link.clone().unwrap().to_header().unwrap().destination,
-		    protocole: value.link.unwrap().to_header().unwrap().ether_type.0,
+		let test_var =value.clone().transport.unwrap();
+		match test_var{
+		    TransportSlice::Tcp(tcp_slice) => {
+
+			let packet = FrontEndPacketData{
+			    source: value.link.clone().unwrap().to_header().unwrap().source,
+			    destination: value.link.clone().unwrap().to_header().unwrap().destination,
+			    protocole: value.link.unwrap().to_header().unwrap().ether_type.0,
+			    source_port: tcp_slice.source_port(),
+			    destination_port: tcp_slice.source_port(),
+			    sequence_number: tcp_slice.sequence_number(),
+			    acknowledgment_number: tcp_slice.acknowledgment_number(),
+			    syn_flag: tcp_slice.syn(),
+			    ack_flag: tcp_slice.ack(),
+			    fin_flag: tcp_slice.fin(),
+			    rst_flag: tcp_slice.rst(),
+			    psh_flag: tcp_slice.psh(),
+			    urg_flag: tcp_slice.urg(),
+			    header_len: tcp_slice.header_len()
+			};
+
+			return Ok(packet)
+		    },
+		    _ => return Err(false)
 		};
-		return Ok(packet)
+
 	    }
 	}
     }
