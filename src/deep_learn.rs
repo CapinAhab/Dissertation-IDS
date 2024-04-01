8c933f4a5990a971466f91eedddb59e6cb2d1a2e
CapinAhab <hazzar.maynard@protonmail.com>
2024-04-01 13:09:25 +0100

    Added LSMT model, needs to be trainable

--- src/deep_learn.rs
+++ src/deep_learn.rs
@@ -1,48 +1,55 @@
-//use tch::{nn, nn::OptimizerConfig, Device, Tensor};
-extern crate tch;
-use tch::{nn, nn::Module, nn::OptimizerConfig, Device, Tensor};
+use burn::{
+    config::Config,
+    module::Module,
+    nn::{
+        conv::{Conv2d, Conv2dConfig},
+        pool::{AdaptiveAvgPool2d, AdaptiveAvgPool2dConfig},
+        Dropout, DropoutConfig, LstmConfig, Lstm, ReLU,
+    },
+    tensor::{backend::Backend, Tensor},
+};
 
-
-//Single layer LSTM may be effective, hinted at in lit
-pub fn single_layer_lstm(){
-    //number of input neurons
-    let input_features = 35;
-
-    //Gets hardware acceleration if possible, otherwise uses CPU
-    let vs = nn::VarStore::new(Device::cuda_if_available());
-
-    //LSTM network
-    let input_layer = nn::lstm(vs.root(), 35, 10, Default::default());
+pub struct DefaultModel<B: Backend> {
+    input_layer:Lstm<B>,
+    output_layer: Lstm<B>,
+    activation: ReLU,
 }
 
+#[derive(Config)]
+pub struct DefaultModelConfig {
+
+    #[config(default = 35)]
+    pub num_features: usize,
+
+    #[config(default = 35)]
+    pub hidden_size: usize,
+}
+
+impl DefaultModelConfig {
+    pub fn init<B: Backend>(&self, device: &B::Device) -> DefaultModel<B> {
+        let input_layer = LstmConfig::new(self.num_features, self.hidden_size, true)
+            .init(device);
+        let output_layer = LstmConfig::new(self.hidden_size, 1, true)
+            .init(device);
+
+        DefaultModel {
+            input_layer,
+            output_layer,
+            activation: ReLU::new(),
+        }
+    }
+}
+/*
+impl<B: Backend> DefaultModel<B> {
+    pub fn forward(&self, input: Tensor<B, 2>) -> Tensor<B, 2> {
+        let x = input.detach();
+        let x = self.input_layer.forward(x);
+        let x = self.activation.forward(x);
+        self.output_layer.forward(x)
+    }
+}
+*/
 
 pub fn gen_net(layers: i64, neurons: i64, lstm_model: bool){
-    //number of input neurons
-    let input_features = 35;
-
-    //Gets hardware acceleration if possible, otherwise uses CPU
-    let vs = nn::VarStore::new(Device::cuda_if_available());
-
-    //Sequential network
-
-    if lstm_model{
-	println!("Not implemented yet");
-
-    }
-    else{
-	//need a fixed number of input neurons to correspond to features
-	let mut net = nn::seq();
-	let input_layer = nn::linear(vs.root(), input_features, neurons, Default::default());
-	net = net.add(input_layer);
-	for _ in 0..layers{
-	    net = net.add_fn(|xs| xs.relu());
-	    net = net.add(nn::linear(vs.root(), neurons, neurons, Default::default()));
-	}
-	
-	let output_layer = nn::linear(vs.root(), neurons, 10, Default::default());
-	net = net.add_fn(|xs| xs.relu());
-	net = net.add(output_layer);
-	
-    }
-    
+    let model = false;
 }
