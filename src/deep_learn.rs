5a265e6571b7ee319c9fdbae843b53741febb5b3
CapinAhab <hazzar.maynard@protonmail.com>
2024-04-05 10:21:30 +0100

    Model can now have multaple layers

--- src/deep_learn.rs
+++ src/deep_learn.rs
@@ -52,7 +52,7 @@ impl<B: Backend> DefaultModel<B> {
 
 pub struct CNNModel<B: Backend> {
     input_layer:Conv1d<B>,
-    output_layer: Conv1d<B>,
+    output_layer: Vec<Conv1d<B>>,
     activation: ReLU,
 }
 
@@ -64,14 +64,20 @@ pub struct CNNModelConfig {
 
     #[config(default = 35)]
     pub hidden_size: usize,
+
+    #[config(default = 1)]
+    pub layers: i64,
 }
 
 impl CNNModelConfig {
     pub fn init<B: Backend>(&self, device: &B::Device) -> CNNModel<B> {
         let input_layer = Conv1dConfig::new(self.num_features, self.hidden_size, 3)
             .init(device);
-        let output_layer = Conv1dConfig::new(self.hidden_size, 1, 3)
-            .init(device);
+        let mut output_layer = Vec::new();
+
+	for _ in 1..self.layers{
+	    output_layer.push(Conv1dConfig::new(self.hidden_size, 1, 3).init(device));
+	}
 
         CNNModel{
             input_layer,
@@ -82,11 +88,14 @@ impl CNNModelConfig {
 }
 
 impl<B: Backend> CNNModel<B> {
-    pub fn forward(&self, input: Tensor<B, 3>) -> Tensor<B, 3> {
-	let x = input; //Dummy variable
-        let x = self.input_layer.forward(x);
-	let x = self.activation.forward(x);
-        self.output_layer.forward(x)
+    pub fn forward(&mut self, input: Tensor<B, 3>) -> Tensor<B, 3> {
+	let mut x = input; //Dummy variable
+        x = self.input_layer.forward(x);
+	for layer in self.output_layer.iter_mut(){
+	    x = self.activation.forward(x);
+	    x = layer.forward(x);
+	}
+	return x
     }
 }
 
